298a0662679d4b583a0f74cf420c22f457bcee4c
Phillip Harris <phillip.harris@atlanticbt.com>
2014-04-25 11:10:50 -0400

    Initial Commit

---
+++ PTTextFieldCell/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PTTextFieldCell
+//
+//  Created by Phillip Harris on 4/25/14.
+//  Copyright (c) 2014 Phillip Harris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ PTTextFieldCellTests/PTTextFieldCellTests.m
@@ -0,0 +1,34 @@
+//
+//  PTTextFieldCellTests.m
+//  PTTextFieldCellTests
+//
+//  Created by Phillip Harris on 4/25/14.
+//  Copyright (c) 2014 Phillip Harris. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PTTextFieldCellTests : XCTestCase
+
+@end
+
+@implementation PTTextFieldCellTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
